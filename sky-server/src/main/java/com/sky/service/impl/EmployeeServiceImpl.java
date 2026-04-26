861b724af374c1945b78b53a868b8378c514bcbc
HuaiFeng <3836231896@qq.com>
2026-04-26 10:51:50 +0800

    添加员工的功能

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +58,29 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    // 新增员工
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        // 转换对象 employeeDTO ---> 转换为 employee 这是给数据库看的
+        Employee employee = new Employee();
+        // 对象属性的拷贝
+        BeanUtils.copyProperties(employeeDTO, employee);
+        // 额外空的属性需要手动设置
+        // 设置账号状态 (软编译)
+        employee.setStatus(StatusConstant.ENABLE);
+        // 设置默认密码 (注意加密)
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        // 设置当前记录时间与修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        // 设置此记录的创建人 id 与修改人id
+        // TODO 后期需要改
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+        // 调用持久层
+        employeeMapper.insert(employee);
     }
 
 }
